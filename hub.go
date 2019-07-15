7578bcde9ef7f9ac0e1afc34e25e370ed70babd8
simonzg <zhanghan.simon@gmail.com>
2019-07-15 20:39:41 +0800

    avoid sending message back to its sender

--- hub.go
+++ hub.go
@@ -8,6 +8,11 @@ import (
 	"fmt"
 )
 
+type MessageEnvelope struct {
+	fromClient string
+	data       []byte
+}
+
 // hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
@@ -15,7 +20,7 @@ type Hub struct {
 	clients map[*Client]bool
 
 	// Inbound messages from the clients.
-	broadcast chan []byte
+	broadcast chan *MessageEnvelope
 
 	// Register requests from the clients.
 	register chan *Client
@@ -26,7 +31,7 @@ type Hub struct {
 
 func newHub() *Hub {
 	return &Hub{
-		broadcast:  make(chan []byte),
+		broadcast:  make(chan *MessageEnvelope),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
@@ -44,10 +49,14 @@ func (h *Hub) run() {
 				close(client.send)
 			}
 		case message := <-h.broadcast:
-			fmt.Println("Broadcast: ", string(message))
+			// fmt.Println("Broadcast: ", string(message))
 			for client := range h.clients {
+				if client.id == message.fromClient {
+					// skip sending message to itself
+					continue
+				}
 				select {
-				case client.send <- message:
+				case client.send <- message.data:
 				default:
 					fmt.Println("close client early: ")
 					close(client.send)
